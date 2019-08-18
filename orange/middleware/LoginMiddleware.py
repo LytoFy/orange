c0a5b33d67ad7a97921c0377636ac570ed060634
wuyeyisheng <wuyeyisheng@qq.com>
2019-08-18 16:40:30 +0800

    pqw 8.18 pm 主要写了用户上传图片，修改必须登录等相关的信息

--- orange/middleware/LoginMiddleware.py
+++ orange/middleware/LoginMiddleware.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import check_password
 from django.core.cache import cache
 from django.http import JsonResponse
 from django.shortcuts import redirect,reverse
@@ -8,12 +9,27 @@ from App.models import User
 
 class LoginMiddleware(MiddlewareMixin):
     def process_request(self,request):
-        path_list = ['/orange/user/info/','/orange/relative/fans/','/orange/relative/concern/','/orange/relative/fanscancle/','/orange/relative/like/','/orange/relative/collection/','/orange/relative/likemural/']  #如果在这个列表就需要验证是否登陆
+        path_list = ['/orange/user/login/','/orange/user/info/','/orange/relative/fans/','/orange/relative/concern/','/orange/relative/fanscancle/','/orange/relative/like/','/orange/relative/collection/','/orange/relative/likemural/', '/orange/home/upload/', '/orange/home/handpeople/']  #如果在这个列表就需要验证是否登陆
         #同url 排除get请求的需要自己加if判断
         if request.path in path_list:
             userid = cache.get('userid')
+
+            print(userid)
             if not userid:
-                return JsonResponse({'msg': '请先登陆', 'code': 0})
+                if request.path == '/orange/user/login/':
+                    email = request.POST.get('email')
+                    password = request.POST.get('password')
+
+                    print(email)
+                    user = User.objects.get(email=email)
+                    if check_password(password, user.password):
+                        request.user = user
+                        cache.set("userid", user.id)
+
+                        return JsonResponse({'msg': '登陆成功', 'code': 1})
+                else:
+                    return JsonResponse({'msg': '请先登陆', 'code': 0})
             else:
                 request.user = User.objects.get(pk = userid)
+
 
