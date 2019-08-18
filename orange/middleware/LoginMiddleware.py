c52be9efdffb1b1e75d72958d11518f4480e0a93
wuyeyisheng <wuyeyisheng@qq.com>
2019-08-18 10:51:25 +0800

    pqw 8.18 am 一些没用的配置文件合并

--- orange/middleware/LoginMiddleware.py
+++ orange/middleware/LoginMiddleware.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.http import JsonResponse
 from django.shortcuts import redirect,reverse
 from  django.utils.deprecation import MiddlewareMixin
@@ -7,13 +8,11 @@ from App.models import User
 
 class LoginMiddleware(MiddlewareMixin):
     def process_request(self,request):
-        path_list = ['/modifyinfo/']  #如果在这个列表就需要验证是否登陆
+        path_list = ['/orange/user/info/','/orange/relative/fans/','/orange/relative/concern/','/orange/relative/fanscancle/','/orange/relative/like/','/orange/relative/collection/','/orange/relative/likemural/']  #如果在这个列表就需要验证是否登陆
         #同url 排除get请求的需要自己加if判断
-
         if request.path in path_list:
-            userid = request.cache.get('userid')
+            userid = cache.get('userid')
             if not userid:
-
                 return JsonResponse({'msg': '请先登陆', 'code': 0})
             else:
                 request.user = User.objects.get(pk = userid)
